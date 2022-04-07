47bbc01c20ca94535463d87c5630df444cece227
allyson_s <allysonsmithstudio@gmail.com>
2022-04-06 20:44:11 -0400

    Fetch api json data, display advice, event listener

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const adviceNumber = document.querySelector(".advice-id");
+const randomQuote = document.querySelector(".random-quote");
+const button = document.querySelector("#dice-button");
+
+//Fetch API JSON Data
+const getData = async function () {
+  const res = await fetch("https://api.adviceslip.com/advice");
+  const data = await res.json();
+  console.log(data);
+  displayAdvice(data);
+};
+
+//Display Advice
+const displayAdvice = function (data) {
+  adviceNumber.innerText = data.slip.id;
+  randomQuote.innerText = data.slip.advice;
+};
+
+//Event listener for button
+button.addEventListener("click", function () {
+  getData();
+});
